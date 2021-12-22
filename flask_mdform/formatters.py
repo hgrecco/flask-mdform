d948cc3c007268754e0db29bd3f698d361a7512c
Hernan <hernan.grecco@gmail.com>
2021-12-21 23:15:37 -0300

    Migrated formatters to newer mdform that uses classes

--- flask_mdform/formatters.py
+++ flask_mdform/formatters.py
@@ -2,11 +2,13 @@
     flask_mdform.formatters
     ~~~~~~~~~~~~~~~~~~~~~~~
 
-    Generate a FlaskForm/WTForm from a Markdown based form.
+    Field formatters for different.
 
-    :copyright: 2020 by flask-mdform Authors, see AUTHORS for more details.
+    :copyright: 2021 by flask-mdform Authors, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
+
+from __future__ import annotations
 
 
 def flask_wtf_bs4(jquery_var="jQuery", wtf_prefix="wtf."):
@@ -22,16 +24,15 @@ def flask_wtf_bs4(jquery_var="jQuery", wtf_prefix="wtf."):
     callable (str, dict) -> str
     """
 
-    def _inner(variable_name, variable_dict):
+    def _inner(variable_name, field):
         args = ["form.%s" % variable_name]
         args.append("form_type='horizontal'")
 
         tag_class = ["form-control"]
-        if variable_dict["nolabel"]:
+        if field.is_label_hidden:
             tag_class.append("nolabel")
 
-        collapse_on = variable_dict.get("collapse_on")
-        if collapse_on:
+        if collapse_on := getattr(field.specific_field, "collapse_on", None):
             if collapse_on.startswith("~"):
                 collapse_on = collapse_on[1:]
                 comparator = "==="
@@ -47,14 +48,14 @@ def flask_wtf_bs4(jquery_var="jQuery", wtf_prefix="wtf."):
 
         args.append('class="%s"' % " ".join("%s" % c for c in tag_class))
 
-        if variable_dict.get("length") is not None:
-            args.append("maxlength=%d" % variable_dict["length"])
+        if length := getattr(field.specific_field, "length", None):
+            args.append("maxlength=%d" % length)
 
         return "{{ %sform_field(%s) }}" % (wtf_prefix, ", ".join(args))
 
     return _inner
 
 
-def flask_wtf(variable_name, variable_dict):
+def flask_wtf(variable_name, field):
     """Formatter that use"""
     return "{{ form.%s.label }} {{ form.%s }}" % (variable_name, variable_name)
